03144f9764cf78b7b9960887c99f66de5f212d16
Luke100000 <jonas.peche@aon.at>
2022-12-29 12:17:16 +0100

    readme

--- changelog.md
+++ changelog.md
@@ -25,6 +25,8 @@
 * You can now start a village with a room (e.g. underground)
 * Added more phrases
 * Fixed villagers using the steve second layer
+* Villager can now open gates (WIP, behave funky)
+* Villager now prefer paths (WIP, potential unstable)
 
 # 7.3.21
 
--- common/src/main/java/net/mca/entity/ai/VillagerNavigation.java
+++ common/src/main/java/net/mca/entity/ai/VillagerNavigation.java
@@ -1,20 +1,9 @@
 package net.mca.entity.ai;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.DoorBlock;
-import net.minecraft.block.FenceGateBlock;
-import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.MobNavigation;
 import net.minecraft.entity.ai.pathing.PathNodeNavigator;
-import net.minecraft.entity.ai.pathing.PathNodeType;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.tag.BlockTags;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-
-import java.util.EnumSet;
 
 public class VillagerNavigation extends MobNavigation {
     public VillagerNavigation(MobEntity mobEntity, World world) {
