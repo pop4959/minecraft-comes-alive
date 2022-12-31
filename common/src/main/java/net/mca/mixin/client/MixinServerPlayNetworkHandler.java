45a563d15ff28e2af1ca5d3622e1753130de52a1
Luke100000 <jonas.peche@aon.at>
2022-12-31 17:07:38 +0100

    improved name search in gpt3

--- common/src/main/java/net/mca/mixin/client/MixinServerPlayNetworkHandler.java
+++ common/src/main/java/net/mca/mixin/client/MixinServerPlayNetworkHandler.java
@@ -15,8 +15,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.text.Normalizer;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,18 +26,23 @@ public class MixinServerPlayNetworkHandler {
     @Shadow
     public ServerPlayerEntity player;
 
+    private String normalize(String string) {
+        return Normalizer.normalize(string, Normalizer.Form.NFD).replaceAll("\\p{M}", "").toLowerCase(Locale.ROOT);
+    }
+
     @Inject(method = "onChatMessage", at = @At("HEAD"))
     public void sendMessage(ChatMessageC2SPacket message, CallbackInfo ci) {
         if (Config.getInstance().enableVillagerChatAI) {
             String msg = StringUtils.normalizeSpace(message.getChatMessage());
             if (!msg.startsWith("/")) {
-                HashSet<String> search = new HashSet<>(Arrays.asList(msg.toLowerCase(Locale.ROOT).split("\\P{L}+")));
                 List<VillagerEntityMCA> entities = WorldUtils.getCloseEntities(player.world, player, 32, VillagerEntityMCA.class);
 
                 // talk to specific villager
+                String search = normalize(msg);
                 boolean talked = false;
                 for (VillagerEntityMCA villager : entities) {
-                    if (search.contains(villager.getTrackedValue(VILLAGER_NAME).toLowerCase(Locale.ROOT))) {
+                    String name = normalize(villager.getTrackedValue(VILLAGER_NAME));
+                    if (search.contains(name)) {
                         PTG3.answer(player, villager, msg, (response) -> {
                             villager.conversationManager.addMessage(player, new LiteralText(response));
                         });
