d6404ba97d1ef79a79c3abae8c02b21293601b3b
Luke100000 <jonas.peche@aon.at>
2023-02-04 00:29:46 +0100

    added to correct listener

--- forge/src/main/java/net/mca/forge/MCAForgeClient.java
+++ forge/src/main/java/net/mca/forge/MCAForgeClient.java
@@ -16,7 +16,6 @@ import net.mca.entity.EntitiesMCA;
 import net.mca.item.BabyItem;
 import net.mca.item.ItemsMCA;
 import net.mca.item.SirbenBabyItem;
-import net.mca.resources.BuildingTypes;
 import net.mca.resources.Supporters;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
@@ -44,7 +43,6 @@ public final class MCAForgeClient {
         ((ReloadableResourceManagerImpl) MinecraftClient.getInstance().getResourceManager()).registerReloader(new MCAScreens());
         ((ReloadableResourceManagerImpl) MinecraftClient.getInstance().getResourceManager()).registerReloader(new ColorPaletteLoader());
         ((ReloadableResourceManagerImpl) MinecraftClient.getInstance().getResourceManager()).registerReloader(new Supporters());
-        ((ReloadableResourceManagerImpl) MinecraftClient.getInstance().getResourceManager()).registerReloader(new BuildingTypes());
     }
 
     @SubscribeEvent
