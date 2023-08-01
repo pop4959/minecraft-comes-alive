8ef7cc3ee166e67d37a170d51fb06a31fe1d169f
Luke100000 <jonas.peche@aon.at>
2023-08-01 10:21:55 +0200

    fixed server crash

--- common/src/main/java/net/mca/mixin/MixinPlayerEntity.java
+++ common/src/main/java/net/mca/mixin/MixinPlayerEntity.java
@@ -1,11 +1,7 @@
 package net.mca.mixin;
 
-import net.mca.Config;
-import net.mca.MCAClient;
 import net.mca.item.BabyItem;
 import net.mca.server.world.data.VillageManager;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
@@ -37,25 +33,6 @@ abstract class MixinPlayerEntity extends LivingEntity {
     private void onDropItem(ItemStack stack, boolean throwRandomly, boolean retainOwnership, CallbackInfoReturnable<ItemEntity> info) {
         if (stack.getItem() instanceof BabyItem baby && !baby.onDropped(stack, (PlayerEntity) (Object) this)) {
             info.setReturnValue(null);
-        }
-    }
-
-    @Inject(method = "getActiveEyeHeight(Lnet/minecraft/entity/EntityPose;Lnet/minecraft/entity/EntityDimensions;)F", at = @At("HEAD"), cancellable = true)
-    public void mca$getActiveEyeHeight(EntityPose pose, EntityDimensions dimensions, CallbackInfoReturnable<Float> cir) {
-        if (Config.getInstance().adjustPlayerEyesToHeight) {
-            MCAClient.getPlayerData(getUuid()).ifPresent(data -> {
-                switch (pose) {
-                    case SWIMMING, FALL_FLYING, SPIN_ATTACK -> {
-                        cir.setReturnValue(0.4f * data.getRawScaleFactor());
-                    }
-                    case CROUCHING -> {
-                        cir.setReturnValue(1.27f * data.getRawScaleFactor());
-                    }
-                    default -> {
-                        cir.setReturnValue(1.62f * data.getRawScaleFactor());
-                    }
-                }
-            });
         }
     }
 }
