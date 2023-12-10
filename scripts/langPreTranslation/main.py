ab4c6f6b97ec6c88b7c61f3c87ba8ea03e800adf
Luke100000 <jonas.peche@aon.at>
2023-12-10 19:59:41 +0100

    added batching

--- scripts/langPreTranslation/main.py
+++ scripts/langPreTranslation/main.py
@@ -2,23 +2,25 @@ import json
 import os
 from json import JSONDecodeError
 from pathlib import Path
+from typing import List
 
 import openai
 from dotenv import load_dotenv
+from tqdm import tqdm
 
 load_dotenv()
 
 
 def generate_phrases(system: str, prompt: str):
     response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4-turbo",
         messages=[
             {"role": "system", "content": system},
             {"role": "user", "content": prompt},
         ],
         temperature=0.8,
         top_p=1,
-        max_tokens=100,
+        max_tokens=200,
     )
     return response.choices[0].message.content
 
@@ -49,7 +51,24 @@ def load_json(file_path: str):
     return {}
 
 
+def to_stem(key: str):
+    return key.split("/")[0]
+
+
+def unflatten_layout(layout: List[str]):
+    keys = {}
+    for key in layout:
+        if key is None:
+            continue
+        stem = to_stem(key)
+        if stem not in keys:
+            keys[stem] = []
+        keys[stem].append(key)
+    return keys
+
+
 layout = read_file(get_lang_path())
+grouped_layout = unflatten_layout(layout)
 
 default_lang = load_json(get_lang_path())
 
@@ -63,23 +82,20 @@ system_prompt = (
 
 
 def get_prompt(key: str, personality: str):
-    return f"Generate a similar phrase for the group `{key}`, but with a {personality} personality:"
+    return f"Generate similar phrases with similar length and exactly the same amount for the group `{key}`, but with a {personality} personality:"
 
 
 def valid_phrase(key: str):
     for phrase in valid_phrases:
-        if key.startswith(phrase):
+        if key.startswith(phrase) and "/" in key:
             return True
     return False
 
 
 personalities = ["grumpy"]
 
-max_gens = 5
-
 
 def process(personality: str):
-    global max_gens
     path = get_lang_path("mca_dialogue_" + personality)
 
     existing_lang = load_json(path)
@@ -89,22 +105,53 @@ def process(personality: str):
     with open(path, "w") as file:
         file.write("{\n")
         newline_required = False
-        for key in layout:
+        processed_groups = set()
+        for key in tqdm(layout, f"Processing {personality}"):
             if key is None:
                 if newline_required:
-                    file.write("\n\n")
+                    file.write("\n")
                     newline_required = False
             else:
                 if valid_phrase(key):
                     if key in existing_lang:
                         file.write(f'  "{key}": "{existing_lang[key]}",\n')
-                    elif max_gens > 0:
-                        max_gens -= 1
-                        generated_text = generate_phrases(
-                            system_prompt,
-                            get_prompt(key, personality) + "\n" + default_lang[key],
-                        )
-                        file.write(f'  "{key}": "{generated_text}",\n')
+                    else:
+                        stem = to_stem(key)
+                        if stem not in processed_groups:
+                            processed_groups.add(stem)
+                            phrases = []
+                            i = 0
+                            for grouped_key in grouped_layout[stem]:
+                                if grouped_key not in existing_lang:
+                                    i += 1
+                                    phrases.append(f"{i}: {default_lang[grouped_key]}")
+
+                            generated_text = generate_phrases(
+                                system_prompt,
+                                get_prompt(key, personality)
+                                + "\n"
+                                + "\n".join(phrases),
+                            )
+                            print("Requested generation.")
+
+                            try:
+                                generated_phrases = [
+                                    (
+                                        phrase.strip().split(":")[1]
+                                        if ":" in phrase
+                                        else phrase.strip()
+                                    )
+                                    for phrase in generated_text.split("\n")
+                                ][-len(phrases) :]
+
+                                assert len(generated_phrases) == len(phrases)
+
+                                for i, grouped_key in enumerate(grouped_layout[stem]):
+                                    file.write(
+                                        f'  "{grouped_key}": "{generated_phrases[i]}",\n'
+                                    )
+                            except Exception as e:
+                                print(e)
                     newline_required = True
         file.write('"_": ""\n')
         file.write("}")
