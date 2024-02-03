22a886fee932616c5c846975159fe08a72a675fc
Luke100000 <jonas.peche@aon.at>
2024-02-03 19:02:32 +0100

    updated lang

--- scripts/langPreTranslation/main.py
+++ scripts/langPreTranslation/main.py
@@ -13,7 +13,6 @@ load_dotenv()
 
 def generate_phrases(system: str, prompt: str):
     response = openai.chat.completions.create(
-        # model="gpt-4-1106-preview",
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": system},
